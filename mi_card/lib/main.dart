29202a5236aa2d341bc0ec414eb81b755425844a
Dadajon <dadajonjurakuziev@gmail.com>
2019-12-12 23:46:43 +0900

    Learn #row and #column layout

--- mi_card/lib/main.dart
+++ mi_card/lib/main.dart
@@ -10,7 +10,34 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                height: 100.0,
+                color: Colors.white,
+                child: Text("Container 1"),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Container(
+                height: 100.0,
+                color: Colors.blue,
+                child: Text("Container 2"),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Container(
+                height: 100.0,
+                color: Colors.red,
+                child: Text("Container 3"),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }

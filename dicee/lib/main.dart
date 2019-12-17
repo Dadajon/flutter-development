2efabb861a108e873a6dc4276fdb66568516fc2d
Dadajon <dadajonjurakuziev@gmail.com>
2019-12-18 01:14:23 +0900

    Start working with #var

--- dicee/lib/main.dart
+++ dicee/lib/main.dart
@@ -18,6 +18,8 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var leftDiceNumber = 5;
+
     return Center(
       child: Row(
         children: <Widget>[
@@ -26,7 +28,7 @@ class DicePage extends StatelessWidget {
               onPressed: () {
                 print('Left button got pressed.');
               },
-              child: Image.asset('images/dice1.png'),
+              child: Image.asset('images/dice$leftDiceNumber.png'),
             ),
           ),
           Expanded(
@@ -34,7 +36,7 @@ class DicePage extends StatelessWidget {
               onPressed: () {
                 print('Right button got pressed.');
               },
-              child: Image.asset('images/dice2.png'),
+              child: Image.asset('images/dice1.png'),
             ),
           ),
         ],

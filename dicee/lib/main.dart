3b8edf1f48fd0ab45282d2084d3102fcfc5c6d16
Dadajon <dadajonjurakuziev@gmail.com>
2019-12-18 01:07:21 +0900

    Add #FlatButton widget and #onPressed function

--- dicee/lib/main.dart
+++ dicee/lib/main.dart
@@ -22,14 +22,18 @@ class DicePage extends StatelessWidget {
       child: Row(
         children: <Widget>[
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
+            child: FlatButton(
+              onPressed: () {
+                print('Left button got pressed.');
+              },
               child: Image.asset('images/dice1.png'),
             ),
           ),
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
+            child: FlatButton(
+              onPressed: () {
+                print('Right button got pressed.');
+              },
               child: Image.asset('images/dice2.png'),
             ),
           ),

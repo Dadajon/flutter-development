e170c0915f4f2166e352d70d893fe61594b4c2c8
Dadajon <dadajonjurakuziev@gmail.com>
2019-12-26 22:13:52 +0900

    Add #main dart file

---
+++ magic-8-ball/lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(BallPage());
+}
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          title: Text('Ask Me Anything'),
+          backgroundColor: Colors.blue.shade900,
+        ),
+        body: SafeArea(
+          child: Ball(),
+        ),
+      ),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Image.asset('images/ball1.png'),
+    );
+  }
+}
